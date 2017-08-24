427125a787ffe5d98de1700434cd364a6f3b8f3b
clouduan <clouduan>
2017-08-24 19:11:08 +0800

    Update

--- SmartQQ.py
+++ SmartQQ.py
@@ -31,7 +31,7 @@ class QQ(HttpHandler):
             self.Download(
                 'https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=0&l=M&s=5&d=72&v=4&t=0.1')
             print("获取二维码成功,请扫描：")
-            Image._show(Image.open('./qrcode.png'))
+            Image.open('./qrcode.png').show()
             while True:
                 html = self.Get(
                     "https://ssl.ptlogin2.qq.com/ptqrlogin?ptqrtoken={ptqrtoken}&webqq_type=10&remember_uin=1&login2qq=1&aid=501004106&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-32750&mibao_css=m_webqq&t=undefined&g=1&js_type=0&js_ver=10197&login_sig=&pt_randsalt=0".format(
